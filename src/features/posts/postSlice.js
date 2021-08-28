42148509fc89102f50df2a3ae9f7f41271f6f6c8
ZRF-star <zhangrf136@nenu.edu.cn>
2021-08-28 13:53:42 +0800

    refactor: 传递参数错误和slice里面写法错误修改

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -23,19 +23,21 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload);
-    },
-    prepare(title, content, userId) {
-      return {
-        payload: {
-          id: nanoid(),
-          date: new Date().toISOString(),
-          title,
-          content,
-          user: userId,
-        },
-      };
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          },
+        };
+      },
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload;
